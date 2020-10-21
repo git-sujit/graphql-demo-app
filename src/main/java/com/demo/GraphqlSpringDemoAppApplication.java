11d9b8ac0d3e149555102062369c24849aaf5e2e
Sujit K Singh <sujit.singh@microsoft.com>
2020-10-20 23:36:58 -0700

    Initial files

---
+++ src/main/java/com/demo/GraphqlSpringDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringDemoAppApplication.class, args);
+	}
+
+}
